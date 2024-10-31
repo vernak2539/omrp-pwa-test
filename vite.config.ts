43b6c7a749642760a02b57a03c4dee918fcac132
Alex Vernacchia <alex.vernacchia@deliveroo.co.uk>
2024-10-31 15:57:15 +0000

    install prettier + add base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,43 @@
-import { VitePWA } from 'vite-plugin-pwa';
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({
-    registerType: 'autoUpdate',
-    injectRegister: false,
+  base: "/omrp-pwa-test/",
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: false,
 
-    pwaAssets: {
-      disabled: false,
-      config: true,
-    },
+      pwaAssets: {
+        disabled: false,
+        config: true,
+        integration: {
+          baseUrl: "https://vernak2539.github.io/omrp-pwa-test/",
+        },
+      },
 
-    manifest: {
-      name: 'omrp-pwa-test',
-      short_name: 'omrp-pwa-test',
-      description: 'omrp-pwa-test',
-      theme_color: '#ffffff',
-    },
+      manifest: {
+        name: "omrp-pwa-test",
+        short_name: "omrp-pwa-test",
+        description: "omrp-pwa-test",
+        theme_color: "#ffffff",
+      },
 
-    workbox: {
-      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
-      cleanupOutdatedCaches: true,
-      clientsClaim: true,
-    },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
 
-    devOptions: {
-      enabled: false,
-      navigateFallback: 'index.html',
-      suppressWarnings: true,
-      type: 'module',
-    },
-  })],
-})
+      devOptions: {
+        enabled: false,
+        navigateFallback: "index.html",
+        suppressWarnings: true,
+        type: "module",
+      },
+    }),
+  ],
+});
