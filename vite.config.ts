8b837f16a5703c64aeb56cb93c0130d7cb666eb5
Alex Vernacchia <alex.vernacchia@deliveroo.co.uk>
2024-11-01 09:51:12 +0000

    try to support legacy browsers

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
 import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import legacy from "@vitejs/plugin-legacy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/omrp-pwa-test/",
   plugins: [
     react(),
+    legacy({
+      targets: ["chrome>=59"],
+      modernTargets: [
+        "edge>=79, firefox>=67, chrome>=59, safari>=12, chromeAndroid>=64, iOS>=12",
+      ],
+    }),
     VitePWA({
       registerType: "autoUpdate",
       injectRegister: false,
